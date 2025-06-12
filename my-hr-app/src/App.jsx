929974ced4f4f51ffc54ce928d0c6cfb1ebd57ed
Alicia <chegealicia8@gmail.com>
2025-06-13 02:25:58 +0300

    first commit

---
+++ my-hr-app/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/employees/')
+      .then((res) => res.json())
+      .then((data) => setEmployees(data))
+      .catch((err) => console.error('Failed to fetch:', err));
+  }, []);
+
+
+  return (
+    
+    <div style={{ padding: '1rem' }}>
+    <h1>Employee List</h1>
+    <ul>
+      {employees.map(emp => (
+        <li key={emp.id}>
+          {emp.user}: {emp.position} in {emp.department}
+        </li>
+      ))}
+    </ul>
+  </div>
+
+     
+
+  )
+}
+
+export default App
